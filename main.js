411918042cc0d91c455466b266f6031c893b6c80
Luzi82 <luzi82@gmail.com>
2015-05-10 17:24:21 +0800

    set the game ratio to 3:4

--- main.js
+++ main.js
@@ -51,14 +51,14 @@ cc.game.onStart = function(){
     if(!cc.sys.isNative && document.getElementById("cocosLoading")) //If referenced loading.js, please remove it
         document.body.removeChild(document.getElementById("cocosLoading"));
         
-    cc.view.setFrameSize(360,640);
+    cc.view.setFrameSize(360,480);
 
     // Pass true to enable retina display, disabled by default to improve performance
     cc.view.enableRetina(false);
     // Adjust viewport meta
     cc.view.adjustViewPort(true);
     // Setup the resolution policy and design resolution size
-    cc.view.setDesignResolutionSize(360, 640, cc.ResolutionPolicy.SHOW_ALL);
+    cc.view.setDesignResolutionSize(360, 480, cc.ResolutionPolicy.SHOW_ALL);
     // The game will be resized when browser size change
     cc.view.resizeWithBrowserSize(true);
     //load resources
