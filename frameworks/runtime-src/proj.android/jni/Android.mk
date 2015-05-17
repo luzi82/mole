6239e565b838f3731a2756308e08ffac22bdc4d3
Luzi82 <luzi82@gmail.com>
2015-05-17 19:56:40 +0800

    fix android build

--- frameworks/runtime-src/proj.android/jni/Android.mk
+++ frameworks/runtime-src/proj.android/jni/Android.mk
@@ -7,9 +7,13 @@ LOCAL_MODULE := cocos2djs_shared
 LOCAL_MODULE_FILENAME := libcocos2djs
 
 LOCAL_SRC_FILES := hellojavascript/main.cpp \
-                   ../../Classes/AppDelegate.cpp 
+                   ../../Classes/AppDelegate.cpp \
+                   ../../../custom/auto/jsb_cocos2dx_cpptest.cpp \
+                   ../../../js-bindings/cocos2d-x/cocos/cpp-test/cpp_test.cpp
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes \
+                    $(LOCAL_PATH)/../../../custom/auto \
+                    $(LOCAL_PATH)/../../../js-bindings/cocos2d-x/cocos/cpp-test
 
 LOCAL_STATIC_LIBRARIES := cocos_jsb_static
 
